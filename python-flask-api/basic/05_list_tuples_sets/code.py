1b9337cad0241d0164ef3b7be69c2697f82f66b3
ln0rd <leonardolsbufalo@gmail.com>
2021-12-26 20:47:07 -0300

    feat: list tuples and sets

---
+++ python-flask-api/basic/05_list_tuples_sets/code.py
@@ -0,0 +1,19 @@
+# List: You can modify the list, insert and remove any element, it keeps the order of elements.
+# Tuple: Is inmutable you can't change anything in a tuple
+# set: Is somethink like a Hashmap or object and you cant have equals elements, it dont have order.
+
+# list
+list = ['Bob', 'Mirian', 'Ash']
+list.append('Smith')
+list.remove('Bob')
+print(list[0])
+
+# tuple
+tuple = ('Bob', 'Mirian', 'Ash')
+print(tuple[2])
+
+# set
+set = {'Bob', 'Mirian', 'Ash'}
+set.add('Eren')
+set.add('Eren')
+print(set)
