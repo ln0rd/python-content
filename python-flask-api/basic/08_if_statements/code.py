d81bc3d9c6c483bda2fb3b627479210ffbbf4173
ln0rd <leonardolsbufalo@gmail.com>
2021-12-26 21:34:43 -0300

    feat: if statement

---
+++ python-flask-api/basic/08_if_statements/code.py
@@ -0,0 +1,8 @@
+day_of_week = input('What day of the week is it today? ').lower()
+
+if day_of_week == 'monday':
+    print('Its Monday')
+elif day_of_week == 'tuesday':
+    print('Its Tuesday')
+else:
+    print('Its not Monday')
