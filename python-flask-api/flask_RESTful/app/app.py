f379d945ea44253fa95cf9379465bda3217ea554
ln0rd <leonardolsbufalo@gmail.com>
2022-01-19 23:12:46 -0300

    feat: new content

--- python-flask-api/flask_RESTful/app/app.py
+++ python-flask-api/flask_RESTful/app/app.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'CHT89012LciotzHga'
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)  # auth
 
 items = []
 
@@ -13,19 +18,29 @@ class Student(Resource):
 
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
-        for item in items:
-            if item.name == name:
-                return item
-        return {'response': 'any item with this name'}
+        # next will return just the first item in this list that I know that just have one item
+        item = next(filter(lambda item: item['name'] == name, items), None)
+        return {'item': item}, 200 if item is not None else 404
 
-    def post(self, name, price):
-        items.append({'name': name, 'price': price})
-        return items
+    def post(self, name):
+        if next(filter(lambda item: item['name'] == name, items), None):
+            return {'message': "item with this name '{}' already exists".format(name)}, 400
+
+        data = request.get_json()
+        items.append({'name': name, 'price': data['price']})
+        return {'items': items}, 201
+
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}, 200
 
 
 # http://127.0.0.1:5000/student/Leo
 api.add_resource(Student, '/student/<string:name>')
 api.add_resource(Item, '/items/<string:name>')
+api.add_resource(ItemList, '/items')
 
-app.run(port=5000)
+app.run(port=5000, debug=True)
